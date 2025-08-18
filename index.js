c16f6ec7ad4fe4167f4705cbc35e935c5c4dab68
AnasIrfan123 <ai8619233@gmail.com>
2025-08-18 11:50:29 +0500

    instruction complete

---
+++ index.js
@@ -0,0 +1,37 @@
+
+//        ********        ASSIGNMENT         *******
+
+//   sir k assignment ki practice kuch is trhn bh ho sakti ha k ap un questions ko 
+//   chatGPT sy bol kar 3 categories me nikalwa sakte hn \
+
+//   *****  Normal   Easy    &    Hard   *****  
+// 
+//  bnakr dega gpt solve kro or 
+// gemini sy question or solve gpt sy agr ap sy solve nh hon tw 
+// or phir un Q ko github par repo sy send krdo 
+
+
+// herry sy bh learning kro  python language 
+
+//        ******  INTERVIEW PREPARATION   *****
+
+// { khushboo mam interview k kuck kuch question btati ha ye interview me pocha jta ha
+// question tw woh bh add krna ha chatgpt or gemini sy nikal kar interview me kia 
+// pocha jta ha or khushboo mam ny btaya bh ha } 
+
+
+// ''  ""  ''' '''
+// single, double & triple qot use case in python please tell me what is working
+
+// string k or list k all method assignment k sath upload github krna ha 
+// string k all method krte hoye k yeh sub krte kia ha / LIST bh
+
+//           **************    ALL METHOD PYTHON    **************
+
+// github me python k all mehtod list or string tuple dictionary set or bh jo ha string data type 
+// or how many data types in python 
+
+// ( sub ko ebglish me pop kia krta ha english me likh lo or sub methods kitny han or ye sub likhdo or kis k agay bracket lgta ha ya nh
+//     ye bh or lga kar github   { All method krte kia han }
+// )
+
