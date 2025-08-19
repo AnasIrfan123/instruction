b0383f1cd8ef5026489baad88ac629297aaedfd7
AnasIrfan123 <ai8619233@gmail.com>
2025-08-19 19:18:38 +0500

    added new folder and file

---
+++ githubStep/step.js
@@ -0,0 +1,18 @@
+
+//                          Git      Steps
+
+
+//     1 )   git init
+//     2 )   git status
+//     3 )   git add -A
+//     4 )   git status
+//     5 )   git config --global user.name ---- => NAME
+//     6 )   git config --global user.email ---- => EMAIL
+//     7 )   git commit -m "work complete"
+//     8 )   git remote add origin --- => REPO LINK
+//     9 )   git push -u origin master/main --- => jo araha ho terminal pr
+
+
+To clone code to existing repo use:
+
+git clone   <REPO LINK>
